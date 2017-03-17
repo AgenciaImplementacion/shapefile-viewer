671161858c21cdaef2a34e47aaf2d3e8dfe8446c
Jorge Useche <naturalmentejorge@gmail.com>
2017-03-17 15:36:37 -0500

    Se estabiliza la versión del build.

---
+++ src/views/README.md
@@ -0,0 +1,3 @@
+In this path would be html files.
+You can create sub-folders to specify the relation with modules, libraries
+or taxonomy.
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './src/main.js',
+const config = {
+  entry: {
+    app: './src/main.js'
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/assets/',
+    path: path.resolve(__dirname, 'build')
+    //publicPath: '/build/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    host: '0.0.0.0',
+    compress: true,
+    port: 9000
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
     new webpack.optimize.UglifyJsPlugin()
-  ]
+  ],
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: 'babel-loader',
+      exclude: path.resolve(__dirname, 'node_modules/')
+    }]
+  }
 };
+
+module.exports = config;
